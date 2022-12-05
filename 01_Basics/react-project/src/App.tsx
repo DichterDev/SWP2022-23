7c45dda05213ab0305d14d30940c21c07db51a2c
DichterDev <david.dr.ruescher@outlook.com>
2022-12-05 08:11:44 +0100

    feat: added basic structure to app

--- 01_Basics/react-project/src/App.tsx
+++ 01_Basics/react-project/src/App.tsx
@@ -4,21 +4,24 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className='todo'>
+        <h2>
+          ToDo
+        </h2>
+        <div className='create-task'>
+
+        </div>
+
+        <div className='tasklist'>
+
+        </div>
+      </div>
+      <div className='completed'>
+        <div className='tasklist'>
+
+        </div>
+      </div>
     </div>
   );
 }
