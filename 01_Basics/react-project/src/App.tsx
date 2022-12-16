7bab0fbaa4c91f91849adb675577cbe01b6ee0aa
DichterDev <david.dr.ruescher@outlook.com>
2022-12-16 16:34:00 +0100

    feat: tweaked application settings

--- 01_Basics/react-project/src/App.tsx
+++ 01_Basics/react-project/src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
+import { ITask } from './components/Task';
+import AddTask from './components/AddTask';
 import './App.css';
+import Tasklist from './components/TaskList';
 
 function App() {
+  const [tasks, _setTasks] = useState<ITask[]>([]);
+
+  function setTasks (_tasks: ITask[]) {
+    _setTasks([..._tasks])
+  }
+
+  function addTask(task:ITask)  {
+    setTasks([...tasks, {...task}]);
+  }
+
+  function removeTask(index:number) {
+    let _tasks = tasks;
+    _tasks.slice(index, 1);
+    setTasks(_tasks);
+  }
+
   return (
     <div className="wrapper">
       <div className='todo'>
         <h2>
           ToDo
         </h2>
-        <div className='create-task'>
-
-        </div>
-
-        <div className='tasklist'>
-
-        </div>
+        <AddTask addTask={addTask}></AddTask>
+        <Tasklist tasklist={[...tasks.filter(task => task.isDone === false)]}></Tasklist>
       </div>
       <div className='completed'>
-        <div className='tasklist'>
-
-        </div>
+        <h2>
+          Completed
+        </h2>
+        <Tasklist tasklist={[...tasks.filter(task => task.isDone === true)]}></Tasklist>
       </div>
     </div>
   );
