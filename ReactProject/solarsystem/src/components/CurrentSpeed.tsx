db9a5339718cae480ea93bb0024a164b6eebfc7f
DichterDev <david.dr.ruescher@outlook.com>
2023-02-28 12:40:04 +0100

    feat: added CurrentSpeed

---
+++ ReactProject/solarsystem/src/components/CurrentSpeed.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useRef, useState } from "react";
+
+function CurrentSpeed() {
+    const [currentSpeed, SetCurrentSpeed] = useState(0);
+
+    const moonRadius = 1737.4 // km
+    const onePixel = 2 * moonRadius // Size of One Pixel  
+
+    let posX = useRef(0);
+    let deltaPosX = useRef(0);
+
+    useEffect(() => {
+        const intervalID = setInterval(() => {
+            deltaPosX.current = (window.scrollX-posX.current < 0) ? window.scrollX-posX.current*-1 : window.scrollX-posX.current;
+            SetCurrentSpeed(Math.round(deltaPosX.current * onePixel* 20));
+            posX.current = window.scrollX;
+        }, 50);
+
+        return () => clearInterval(intervalID);
+    });
+
+    return (
+        <div className="current-speed">{currentSpeed}</div>
+    );
+}
+
+export default CurrentSpeed;
