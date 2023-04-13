7fe8995ddc613d433c41acaea0d462d9e3383d90
DichterDev <david.dr.ruescher@outlook.com>
2023-04-13 11:02:28 +0200

    feat: refactor remove unused code

--- ReactProject/solarsystem/src/components/Navigation.tsx
+++ ReactProject/solarsystem/src/components/Navigation.tsx
@@ -12,27 +12,18 @@ interface props {
 }
 
 function Navigation(props: props) {
-
-  const [imageModal, setImageModal] = useState("");
-
   function handleClick(name: string) {
-    // TODO: Scroll to object with `name`
     let objectElement = document.getElementById(name);
     objectElement?.scrollIntoView({behavior: "smooth", block: 'center', inline: 'center'})
-  }
-
-  function handleHover(name: string) {
-    setImageModal(name);
   }
 
   return(
     <div className={styles.navigation}>
       {
         props.objects.map(object => {
-          return <img src={require(`../icons/${object.name}.png`)} alt={object.name} onClick={() => handleClick(object.name)} onMouseOver={() => handleHover(object.name)}></img>
+          return <img src={require(`../icons/${object.name}.png`)} alt={object.name} onClick={() => handleClick(object.name)}></img>
         })
       }
-      <div style={{color: 'white'}}>{imageModal}</div>
       {<CurrentSpeed></CurrentSpeed>}
 
     </div>  
